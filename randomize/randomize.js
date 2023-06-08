da693ecf9d6c1dfd7dbd3de7fea0a22770b752e6
ProbeHub <133328910+ProbeHub@users.noreply.github.com>
2023-06-08 16:55:05 +0800

    Add files via upload
    randomization

---
+++ randomize/randomize.js
@@ -0,0 +1,82 @@
+let numInputs = 1;
+
+function addInput() {
+	numInputs++;
+
+	const inputsContainer = document.getElementById("inputs-container");
+	const newInput = document.createElement("div");
+
+	newInput.innerHTML = `<label for="alphabets-input-${numInputs}">Swallowing Trial:</label>
+	<input type="text" id="alphabets-input-${numInputs}" data-index="${numInputs}">
+	<button onclick="changeIncrement(${numInputs}, 1)">+</button>
+	<button onclick="changeIncrement(${numInputs}, -1)">-</button>
+	<input type="text" id="alphabets-input-count-${numInputs}" disabled>
+	<br><br>`;
+
+	inputsContainer.appendChild(newInput);
+}
+
+function changeIncrement(index, increment) {
+	const input = document.getElementById(`alphabets-input-${index}`);
+	const countField = document.getElementById(`alphabets-input-count-${index}`);
+	let currentIncrement = parseInt(input.getAttribute("data-increment")) || 1;
+	currentIncrement += increment;
+	if (currentIncrement < 1) {
+		currentIncrement = 1;
+	}
+	input.setAttribute("data-increment", currentIncrement);
+	countField.value = `Number of repetition: ${currentIncrement}`;
+}
+
+function randomize() {
+	const inputs = document.getElementsByTagName("input");
+	const output = document.getElementById("output");
+
+	// Create an object to store the input values and their respective increment counters
+	const inputValues = {};
+	for (let i = 0; i < inputs.length; i++) {
+		if (inputs[i].type === "text") {
+			const index = inputs[i].getAttribute("data-index");
+			inputValues[index] = inputs[i].value;
+		}
+	}
+
+	// Concatenate the input values into a single string, duplicating each input by its designated increment number
+	let concatenatedAlphabets = "";
+	for (let i = 1; i <= numInputs; i++) {
+		if (inputValues[i]) {
+			const input = inputValues[i];
+			const increment = parseInt(document.getElementById(`alphabets-input-${i}`).getAttribute("data-increment")) || 1;
+			for (let j = 0; j < increment; j++) {
+				concatenatedAlphabets += input + ',';
+			}
+		}
+	}
+
+	// Split the concatenated string into an array of characters
+	const alphabets = concatenatedAlphabets.split(",");
+
+	// Randomize the array
+	for (let i = alphabets.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[alphabets[i], alphabets[j]] = [alphabets[j], alphabets[i]];
+	}
+
+	// Count the occurrences of each letter
+	let outputAlphabets = "";
+	const counts = {};
+	for (let i = 0; i < alphabets.length; i++) {
+		if (alphabets[i] == "") { continue; }
+		const letter = alphabets[i];
+		if (counts[letter]) {
+			counts[letter]++;
+		} else {
+			counts[letter] = 1;
+		}
+		outputAlphabets += alphabets[i] + " trial_" + counts[letter] + "\n";
+	}
+
+	// Set the randomized array as the output value
+	// output.value = alphabets.join("\n");
+	output.value = outputAlphabets;
+}
